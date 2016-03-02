09853b07ae4718eec538186bc3073afd36580ada
pwnator <joel_savier_ng@yahoo.com>
2016-03-02 11:34:04 +0800

    modified:   testplot/settings.py 	modified:   testplot/wsgi.py

--- testplot/settings.py
+++ testplot/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '06*bs&49@*^wtqwqa@2bia^om37wpiz#!u=3knjl&4mdg%a723'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['db.portal.piic.org.ph','10.10.25.176','localhost']
 
 
 # Application definition
@@ -99,6 +99,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
