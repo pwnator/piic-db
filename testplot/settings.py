2c6505cfca0c22628ea1d9dd749f78421260501e
pwnator <joel_savier_ng@yahoo.com>
2016-03-09 15:49:27 +0800

    modified:   testplot/settings.py 	modified:   testplot/urls.py 	modified:   testplot/wsgi.py

--- testplot/settings.py
+++ testplot/settings.py
@@ -100,7 +100,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+if DEBUG:
+	STATIC_ROOT = os.path.join(BASE_DIR,'/static')
+else:
+	STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
--- testplot/urls.py
+++ testplot/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('plotter.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.STATIC_ROOT, 'show_indexes' : settings.DEBUG}),
 )
 
 if not settings.DEBUG:
